04ed360d6e8773768dfb6f87cf44cb1954e5105f
최서현 <jwsh171210@naver.com>
2023-03-29 14:52:10 +0900

    start

---
+++ 0329.py
@@ -0,0 +1,44 @@
+# 0329
+'''
+print("안녕하세요")
+print(3)
+print(10, 6)
+
+var = "안녕"
+print(var)
+print(type(var))
+print(type(3))
+
+var1 = 3 #서로의 자리를 바꾸면 에러남
+height = 180
+weight=50
+radius = 
+'''
+
+'''
+a = 3
+b = 10
+
+c = a + b
+a = c
+a = a + b
+a += b
+
+a*=b 
+
+print ("a : ", a)
+'''
+'''
+a = 2
+b = 3
+a **= b
+print(a)
+'''
+
+
+name = input("이름이 뭔가요?")
+print("제 이름은 ", name, "입니다. ")
+age = input("나이는?")
+print("나이는", age , "입니다.")
+# print ("아버지 나이는", age + 30, "입니다. ")
+print(type(age))
